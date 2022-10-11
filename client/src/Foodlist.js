d8a3012bcdb6efa06bed0d1754f68c9b8292bcb3
Edsey omenda <omendajames0317@gmail.com>
2022-10-11 16:40:54 +0300

    updates the component to use cards to hold burger image

--- client/src/Foodlist.js
+++ client/src/Foodlist.js
@@ -3,6 +3,15 @@ import React from "react";
 export default function Foodlist(){
 
     return(
-        <div></div>
+        <div>
+            <div className='card' style={{width : 25 + 'rem'}} > 
+            <img src={burgerUrl} className="card-img-top" alt={burgerTitle}
+            />
+            <div className='card-body'>
+            <h5 className="card-title" >{burgerTitle}</h5>
+                {/* <Link to={"/game/"+ id} className="btn btn-info">Game Description</Link> */}
+            </div>
+        </div>
+        </div>
     )
 }
