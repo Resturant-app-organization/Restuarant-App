b7e53eb56b696345bc0afdf068e930ece548c6b9
laureen <laureen.akinyi@student.moringaschool.com>
2022-10-11 17:42:59 +0300

    generated rails resource for cjrestaurant

---
+++ app/models/cjrestaurant.rb
@@ -0,0 +1,2 @@
+class Cjrestaurant < ApplicationRecord
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :cjrestaurants
   resources :burgers
   resources :reviews
   resources :customers
