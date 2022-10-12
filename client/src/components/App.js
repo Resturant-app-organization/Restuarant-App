99ba8f748e7ccaa44c3746d8e772d2302d45fdf6
Edsey omenda <omendajames0317@gmail.com>
2022-10-12 10:47:32 +0300

    updates changes

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,21 +1,22 @@
 import React from 'react';
-import Navbar from './Navbar';
-import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
-import Burgers from './Burgers';
-import BurgerDescription from './Details';
+// import Navbar from './Navbar';
+// import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+// import Burgers from './Burgers';
+// import BurgerDescription from './Details';
 
 function App() {
   return (
-    <Router>
-       <div className="App">
-    <Navbar />
-    <h1>Welcome to burger hub</h1>
-    </div>
-    <Routes>
-      <Route  exact path="/burgers" element={<Burgers />}></Route>
-      <Route exact path="/burger/:id" element={<BurgerDescription />}></Route>
-    </Routes>
-    </Router>
+    <div></div>
+    // <Router>
+    //    <div className="App">
+    // <Navbar />
+    // <h1>Welcome to burger hub</h1>
+    // </div>
+    // <Routes>
+    //   <Route  exact path="/burgers" element={<Burgers />}></Route>
+    //   <Route exact path="/burger/:id" element={<BurgerDescription />}></Route>
+    // </Routes>
+    // </Router>
    
   );
 }
--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,16 +1,17 @@
 import React from 'react';
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
 
 export default function Navbar(){
     return (
-        <nav className=' navbar-expand-lg bg-light'>
-            < div className='container-fluid'>
-                <ul className="nav nav-pills mb-3" id="pills-tabContent" role="tablist">
-                    < li className='nav-item" role="presentation'>
-                    <Link className='nav-link ' to="/burgers">Burgers</Link>
-                    </li>
-                </ul>
-            </div>
-        </nav>
+        <div></div>
+        // <nav className=' navbar-expand-lg bg-light'>
+        //     < div className='container-fluid'>
+        //         <ul className="nav nav-pills mb-3" id="pills-tabContent" role="tablist">
+        //             < li className='nav-item" role="presentation'>
+        //             <Link className='nav-link ' to="/burgers">Burgers</Link>
+        //             </li>
+        //         </ul>
+        //     </div>
+        // </nav>
     )
 }
