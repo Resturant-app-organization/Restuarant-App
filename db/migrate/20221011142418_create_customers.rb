a0cfc07a2401cea1a4baaad867d5195c33c0a71e
+ <+>
2022-10-12 14:18:00 +0300

    updating migrations and controllers

--- db/migrate/20221011142418_create_customers.rb
+++ db/migrate/20221011142418_create_customers.rb
@@ -1,7 +1,8 @@
 class CreateCustomers < ActiveRecord::Migration[7.0]
   def change
     create_table :customers do |t|
-      t.string :name
+      t.string :username
+      t.string :password_digest
       t.integer :phone_number
       t.string :gender
 
