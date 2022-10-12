0670bf94513729e1a5ab39dd0c75efe69c3c7ed6
laureen <laureen.akinyi@student.moringaschool.com>
2022-10-12 20:50:02 +0300

    adding validations

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,4 +1,7 @@
 class Customer < ApplicationRecord
     has_many :reviews 
     has_many :burgers
+
+    validates :username, presence: true, uniqueness: true
+    validates :description, {:length => {:minimum => 20}}
 end
