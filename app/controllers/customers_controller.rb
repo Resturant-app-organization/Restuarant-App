01436d9d07d0d360c145c1012d74bb671ed75d09
laureen <laureen.akinyi@student.moringaschool.com>
2022-10-13 16:42:36 +0300

    made changes to reviews

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,15 +1,20 @@
 class CustomersController < ApplicationController
-    skip_before_action :authorize, only: :create
+    # skip_before_action :authorize, only: [:create, :show]
 
     def create
         customer = Customer.create!(customer_params)
-        Session[:customer_id] = customer.id
+        # Session[:customer_id] = customer.id
         render json: customer, status: :created
     end
 
+    # def show
+    #     render json: @current_customer
+    # end
+
     def show
-        render json: @current_customer
-    end
+        customer = Customer.find_by!(id: params[:id])
+        render json: customer, status: :ok
+    end 
 
     private
 
